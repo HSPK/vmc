e8192f5876fc668de102cd0d61d458dca6d3c8c2
HSPK <whxway@whu.edu.cn>
2024-11-04 21:23:55 +0800

    remove request params context

--- vmc/context/params.py
+++
@@ -1,5 +0,0 @@
-from contextvars import ContextVar
-from typing import Dict
-
-# The context variable for the current request.
-request_params: ContextVar[Dict] = ContextVar("request_params", default={})
