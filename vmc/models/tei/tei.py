7d5d367f924b6ce7807be55a841c8885ae262a17
HSPK <whxway@whu.edu.cn>
2024-11-13 23:09:49 +0800

    add batch size for tei embedding

--- vmc/models/tei/tei.py
+++ vmc/models/tei/tei.py
@@ -18,19 +18,23 @@ class TeiEmbedding(BaseEmbeddingModel, BaseRerankModel):
     async def embedding(
         self,
         content: Union[str, List[str], Iterable[int], Iterable[Iterable[int]]],
+        batch_size: int = 512,
         **kwargs,
     ) -> EmbeddingResponse:
         if kwargs:
             logger.warning(f"{self.model_id} Unused parameters: {kwargs}")
         created = time.time()
-        res = await self.client.post(
-            "/embed",
-            body={"inputs": content},
-            options={"raw_response": True},
-            cast_to=list[list[int]],
-        )
+        embeddings = []
+        for i in range(0, len(content), batch_size):
+            res = await self.client.post(
+                "/embed",
+                body={"inputs": content[i : i + batch_size]},
+                options={"raw_response": True},
+                cast_to=list[list[int]],
+            )
+            embeddings.extend(res.json())
         return EmbeddingResponse(
-            embedding=res.json(),
+            embedding=embeddings,
             created=created,
             embed_time=time.time() - created,
             model=self.model_id,
